5532b0096feb30a472d8141a9612f1ab822aac79
granthopperflash <98110041+granthopperflash@users.noreply.github.com>
2022-04-21 19:19:38 +0000

    Commit

--- sales-upload/__init__.py
+++ sales-upload/__init__.py
@@ -20,49 +20,53 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         except ValueError:
             pass
         else:
-            name = req_body.get('uri')
+            uri = req_body.get('uri')
 
     if not csvSAS:
         csvSAS = req_body.get('sas')
 
-    xls_file = xlsURI[xlsURI.find('MISALES/') + 8:]
+    if xlsURI and csvSAS:
 
-    if xls_file.upper().endswith('.XLS'):
+        xls_file = xlsURI[xlsURI.find('MISALES/') + 8:]
 
-        csv_file = xls_file[:-4] + '.csv'
-        csv_rows = []
-        row = []
+        if xls_file.upper().endswith('.XLS'):
 
-        with open(xlsURI) as xls_object:
-            csv_lines = csv.DictReader(xls_object, delimiter=',')
+            csv_file = xls_file[:-4] + '.csv'
+            csv_rows = []
+            row = []
 
-            for line in csv_lines:
-                if line['Unnamed: 0'].find('Period From') >= 0:
-                    rpt_date = line['Unnamed: 0'][-10:]
+            with open(xlsURI) as xls_object:
+                csv_lines = csv.DictReader(xls_object, delimiter=',')
 
-                if not line['Unnamed: 0'].isdigit():
-                    if line['Unnamed: 0'].find(' - ') >= 0:
-                        loc_id = xls_file[11:13]
+                for line in csv_lines:
+                    if line['Unnamed: 0'].find('Period From') >= 0:
+                        rpt_date = line['Unnamed: 0'][-10:]
 
-                    elif line['Unnamed: 5'].find('Disc') >= 0:
-                        row = ['0555555', 'Discount', 1, line['Unnamed: 8'], rpt_date, loc_id, date.today()]
+                    if not line['Unnamed: 0'].isdigit():
+                        if line['Unnamed: 0'].find(' - ') >= 0:
+                            loc_id = xls_file[11:13]
 
-                else:
-                    if line['Unnamed: 0'] is not None:
-                        item_id = line['Unnamed: 0'].zfill(7)
-                        row = [item_id, line['Unnamed: 1'], line['Unnamed: 4'], line['Unnamed: 8'], rpt_date, loc_id, date.today()]
+                        elif line['Unnamed: 5'].find('Disc') >= 0:
+                            row = ['0555555', 'Discount', 1, line['Unnamed: 8'], rpt_date, loc_id, date.today()]
 
-                if row:
-                    csv_rows.append(row)
+                    else:
+                        if line['Unnamed: 0'] is not None:
+                            item_id = line['Unnamed: 0'].zfill(7)
+                            row = [item_id, line['Unnamed: 1'], line['Unnamed: 4'], line['Unnamed: 8'], rpt_date, loc_id, date.today()]
 
-        if row:
-            df = pd.DataFrame(csv_rows)
+                    if row:
+                        csv_rows.append(row)
 
-            blob_container = ContainerClient.from_container_url(csvURI + csvSAS)
-            blob_client = blob_container.get_blob_client(csv_file)
-            blob_client.upload_blob(data=df.to_csv(index=False, header=False))
+            if row:
+                df = pd.DataFrame(csv_rows)
 
-            return_dict['func_return'] = 'True'
+                blob_container = ContainerClient.from_container_url(csvURI + csvSAS)
+                blob_client = blob_container.get_blob_client(csv_file)
+                blob_client.upload_blob(data=df.to_csv(index=False, header=False))
+
+                return_dict['func_return'] = 'True'
+            else:
+                return_dict['func_return'] = False
 
     else:
         return_dict['func_return'] = False
