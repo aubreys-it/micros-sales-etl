74a71de6833e6fedeb7211bba072bff459fc558a
granthopperflash <98110041+granthopperflash@users.noreply.github.com>
2022-04-22 16:59:16 +0000

    Commit

--- sales-upload/__init__-DESKTOP-AA20NNI.py
+++ sales-upload/__init__-DESKTOP-AA20NNI.py
@@ -5,6 +5,7 @@ import pandas as pd
 import azure.functions as func
 from azure.storage.blob import ContainerClient
 from datetime import date
+from smart_open import open
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
